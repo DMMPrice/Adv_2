d43e7af171fdc60005c26f4b29876efea8b8dcab
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2022-06-30 18:46:23 +0530

    name file done

--- States_and_districts.py
+++ States_and_districts.py
@@ -23,3 +23,44 @@ district_4 = ["Araria", "Arwal", "Aurangabad(BH)", "Banka", "Begusarai", "Bhagal
               "Muzaffarpur", "Nalanda", "Nawada,"    "Patna", "Purnia", "Rohtas",
               "Saharsa", "Samastipur", "Saran", "Sheikhpura", "Sheohar", "Sitamarhi", "Siwan", "Supaul", "Vaishali",
               "West Champaran"]
+district_5 = ["Bastar", "Bijapur(CGH)", "Bilaspur(CGH)", "Dantewada", "Dhamtari", "Durg", "Gariaband",
+              "Janjgir - champa", "Jashpur",
+              "Kanker", "Kawardha", "Korba", "Koriya", "Mahasamund",
+              "Narayanpur", "Raigarh", "Raipur", "Rajnandgaon", "Surguja"]
+district_6 = ["North Goa", "South Goa"]
+district_7 = ["Ahmedabad", "Amreli", "Anand", "Banaskantha", "Bharuch", "Bhavnagar", "Dahod", "Gandhi Nagar",
+              "Jamnagar", "Junagadh", "Kachchh", "Kheda", "Mahesana", "Narmada",
+              'Navsari', 'Panch Mahals', 'Patan', 'Porbandar', 'Rajkot', "Sabarkantha", "Surat", "Surendra Nagar",
+              "Tapi", "The Dangs", "Vadodara", "Valsad"]
+district_8 = ['Ambala', 'Bhiwani', 'Faridabad', 'Fatehabad', 'Gurgaon', 'Hisar', 'Jhajjar', 'Jind',
+              'Kaithal', "Karnal", "Kurukshetra", "Mahendragarh,""Panchkula", "Panipat",
+              'Rewari', 'Rohtak', 'Sirsa', 'Sonipat', 'Yamuna Nagar']
+district_9 = ['Bilaspur (HP)', 'Chamba', 'Hamirpur(HP)', 'Kangra', 'Kinnaur', 'Kullu', 'Lahul & Spiti', 'Mandi',
+              'Shimla', 'Sirmaur', 'Solan', 'Una']
+district_10 = ['Ananthnag', 'Bandipur', 'Baramulla', 'Budgam', 'Doda', 'Jammu', 'Kargil', 'Kathua', 'Kulgam', 'Kupwara',
+               'Leh', 'Poonch',
+               'Pulwama', 'Rajauri', 'Reasi', 'Shopian',
+               'Srinagar', 'Udhampur']
+district_11 = [Bokaro, Chatra, Deoghar, Dhanbad, Dumka, East Singhbhum, Garhwa, Giridh, Godda, Gumla, Hazaribag,
+               Jamtara, Khunti,
+               Koderma, Latehar, Lohardaga, Pakur, Palamau, Ramgarh, Ranchi, Sahibganj, Seraikela - kharsawan, Simdega,
+               West Singhbhum]
+district_12 = [Bagalkot, Bangalore, Bangalore Rural, Belgaum, Bellary, Bidar, Bijapur(KAR), Chamrajnagar, Chickmagalur,
+               Chikkaballapur, Chitradurga,
+               Dakshina Kannada, Davangere, Dharwad, Gadag, Gulbarga, Hassan, Haveri, Kodagu, Kolar, Koppal, Mandya,
+               Mysore, Raichur, Ramanagar, Shimoga,
+               Tumkur, Udupi, Uttara Kannada, Yadgir]
+district_13 = [Alappuzha, Ernakulam, Idukki, Kannur, Kasargod, Kollam, Kottayam, Kozhikode, Malappuram, Palakkad,
+               Pathanamthitta, Thiruvananthapuram, Thrissur, Wayanad]
+district_14 = [Alirajpur, Anuppur, Ashok Nagar, Balaghat, Barwani, Betul, Bhind, Bhopal, Burhanpur, Chhatarpur,
+               Chhindwara, Damoh,
+               Datia, Dewas, Dhar, Dindori, East Nimar, Guna, Gwalior, Harda, Hoshangabad, Indore, Jabalpur,
+               Jhabua, Katni, Khandwa, Khargone, Mandla, Mandsaur, Morena, Narsinghpur, Neemuch, Panna, Raisen, Rajgarh,
+               Ratlam,
+               Rewa, Sagar, Satna, Sehore, Seoni, Shahdol, Shajapur, Sheopur, Shivpuri, Sidhi, Singrauli, Tikamgarh,
+               Ujjain, Umaria, Vidisha,
+               West Nimar]
+district_15 = [Ahmed Nagar, Akola, Amravati, Aurangabad, Beed, Bhandara, Buldhana, Chandrapur, Dhule, Gadchiroli,
+               Gondia, Hingoli,
+               Jalgaon, Jalna, Kolhapur, Latur, Mumbai, Nagpur, Nanded, Nandurbar, Nashik, Osmanabad, Parbhani, Pune,
+               Raigarh(MH), Ratnagiri, Sangli, Satara, Sindhudurg, Solapur, Thane, Wardha, Washim, Yavatmal]
--- name_and_gender.py
+++ name_and_gender.py
@@ -1,4 +1,14 @@
 import random
-from getindianname import *
+import indian_names as name
 
-print(random.randint(1, 3))
+
+def name_and_gender():
+    choice = random.randint(1, 2)
+    name_gender = []
+    if choice == 1:
+        name_gender.append(name.get_full_name(gender='male'))
+        name_gender.append("male")
+    else:
+        name_gender.append(name.get_full_name(gender='female'))
+        name_gender.append("female")
+    return name_gender
